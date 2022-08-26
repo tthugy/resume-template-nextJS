e7950ee2b1845c9474f8109e5f1c94a22d4d8f43
tthugy <supermanace7@gmail.com>
2022-08-27 00:02:21 +0900

    update

---
+++ pages/header.js
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image'
+import { useRouter } from "next/router";
+import styles from '../styles/header.module.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import memoticon from '../public/memoticon.png'
+
+export default function Header() {
+
+  useEffect(() => {
+    AOS.init();
+  })
+
+  return (
+    <header>
+        <span data-aos="fade-up" data-aos-duration="800" className={styles.tIcon}>
+          <div className={styles.memo}>
+            <Image alt="Wonseok" src={memoticon} layout='responsive' objectFit='contain' priority />
+          </div>
+        </span>
+        <div data-aos="fade-up" data-aos-duration="800" className={styles.title}>
+          <p>Resume of <span>Lorem ipsum</span></p>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="800" className={`${styles.linkCollect}`}>
+            <a target='_blank' href='https://github.com/tthugy' rel='noopener noreferrer'>
+              <i className="bi bi-github"></i>
+            </a>
+            <a target='_blank' href='https://www.linkedin.com/in/wonseok-choi-b6041922b/' rel='noopener noreferrer'>
+              <i className="bi bi-linkedin"></i>
+            </a>
+            <a href='mailto:cwstwin1@gmail.com'>
+              <i className={`bi bi-at`}></i>
+            </a>
+        </div>
+    </header>
+  )
+}
