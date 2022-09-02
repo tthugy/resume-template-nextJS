d9972f0264199f92a7bc6c37470aba6192c64f68
tthugy <supermanace7@gmail.com>
2022-09-03 00:31:36 +0900

    update

---
+++ pages/toast.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import React, { useState, useEffect, useRef } from 'react';
+import { atom } from 'recoil';
+import Header from './header'
+import Main from './main'
+
+export default function Toast() {
+
+  const [toastStatus, setToastStatus] = useState(false);
+
+  const toastPop = () => {
+    setToastStatus(true);
+
+    setTimeout(()=> {
+      setToastStatus(false);
+    }, 1000)
+  }
+
+  return (
+    <>
+        <div class="">
+        </div>
+    </>
+  )
+}
